a685dbbe8bfc3cc22faf57039d3cdb2ef88dfb9e
Rob Dietrick <rdietrick@github.com>
2020-04-22 19:06:08 -0700

    more debugging

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,9 @@ PATHSPEC=$2
 echo "Checking diffs in commit $SHA"
 
 echo "executing: git diff-tree --no-commit-id --name-only -r $SHA $PATHSPEC"
-git diff-tree --no-commit-id --name-only -r $SHA $PATHSPEC
+
+RES=`git diff-tree --no-commit-id --name-only -r $SHA $PATHSPEC`
+echo "RESULTS: $RES"
 
 function check() {
   if [[ -z "$(git diff-tree --no-commit-id --name-only -r $SHA $PATHSPEC)" ]];
