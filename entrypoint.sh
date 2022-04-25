660bd4eb62bbe42eee89917bcb417d701d5cecf9
Rob Dietrick <rdietrick@github.com>
2022-04-25 15:24:35 -0700

    attempt to add safe directory

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,12 +4,12 @@ set -e
 SHA=$1
 PATHSPEC=$2
 
-echo "Debugging info"
-pwd
-ls -la /
-ls -la .
-ls -la ../
-#git config --global --add safe.directory /github/workspace
+# echo "Debugging info"
+# pwd
+# ls -la /
+# ls -la .
+# ls -la ../
+git config --global --add safe.directory "$GITHUB_WORKSPACE"
 
 function check() {
   if [[ -z "$(git diff-tree --no-commit-id --name-only -m -r $SHA $PATHSPEC)" ]];
